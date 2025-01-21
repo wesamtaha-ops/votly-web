8e1d9440a3b96174e7ab05f3865b9dad052a5b27
Mohamad Alalwan <mohamad.alalwan@altherr.de>
2025-01-22 02:17:12 +0400

    debug

---
+++ src/app/api/userProfileAnswers/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { callVotlyApi } from "../../helper";
+
+export async function GET(req) {
+  const userToken = req.headers.get("userToken");
+
+  const res = await callVotlyApi({
+    type: "get",
+    url: "v2/user/profile/answers",
+    data: {},
+    userToken: userToken,
+  });
+
+  return NextResponse.json(res);
+}
+
+export async function POST(req) {
+  const userToken = req.headers.get("userToken");
+  const payload = req.body;
+
+  const res = await callVotlyApi({
+    type: "post",
+    url: "v2/user/profile/answers",
+    data: payload,
+    userToken: userToken,
+  });
+
+  return NextResponse.json(res);
+}
