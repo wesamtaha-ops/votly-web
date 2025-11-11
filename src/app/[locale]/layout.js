0b8aec383b1ed67b1fc82aee4a3b66d28daf46a8
WESAM TAHA <tahawesam@yahoo.com>
2025-11-11 22:33:04 +0100

    google and hotjar

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -32,6 +32,36 @@ export default function RootLayout({ children, params }) {
 
   return (
     <html lang={locale} dir={isRTL ? 'rtl' : 'ltr'} className={`${almarai.variable}`}>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TWKSZ0CR1R"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TWKSZ0CR1R');
+            `,
+          }}
+        />
+        
+        {/* Hotjar Tracking Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:397196,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
+      </head>
       <body className={isRTL ? almarai.className : inter.className}>
         <SessionProvider>
           <NextIntlClientProvider locale={locale} messages={messages}>
